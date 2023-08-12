e4f1647b1ac5d7810e61d31abe432de9e7eb0f7e
平佐竜也 <78416683+hirasatatsuya@users.noreply.github.com>
2023-08-12 21:47:57 +0900

    Add files via upload

---
+++ mysite/myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class item(models.Model):
+    code = models.IntegerField()
+    name = models.TextField()
+    price = models.IntegerField()
+
+
